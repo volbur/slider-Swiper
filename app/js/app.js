d89bdbc5cb3243763adef5eae607ab16070a1a14
Volodymyr Burkovskiy <volodymyr.b.pro@gmail.com>
2021-08-08 09:52:59 +0300

    Add new slider and synchronization of two sliders

--- app/js/app.js
+++ app/js/app.js
@@ -1,13 +1,21 @@
-import { Swiper, Parallax, Mousewheel } from "swiper";
-Swiper.use([Parallax, Mousewheel]);
+import { Swiper, Parallax, Mousewheel, Controller } from "swiper";
+Swiper.use([Parallax, Mousewheel, Controller]);
 
 document.addEventListener("DOMContentLoaded", () => {
   const swiperIMG = new Swiper(".slider-img", {
     loop: false,
     speed: 2500,
     parallax: true,
+  });
+
+  const swiperText = new Swiper(".slider-text", {
+    loop: false,
+    speed: 2500,
     mousewheel: {
       invert: false,
     },
   });
+
+  swiperIMG.controller.control = swiperText;
+  swiperText.controller.control = swiperIMG;
 });
