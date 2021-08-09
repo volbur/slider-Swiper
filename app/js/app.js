08fd1736b280027778d6744408c3c5338ca8fb3f
Volodymyr Burkovskiy <volodymyr.b.pro@gmail.com>
2021-08-09 07:21:45 +0300

    Add event - 'slideChange'

--- app/js/app.js
+++ app/js/app.js
@@ -19,10 +19,19 @@ Swiper.use([
 import { gsap, Power2 } from "gsap";
 
 document.addEventListener("DOMContentLoaded", () => {
+  //Swiper
+
   const swiperIMG = new Swiper(".slider-img", {
     loop: false,
     speed: 2500,
     parallax: true,
+    pagination: {
+      el: ".slider-pagination-count .total",
+      type: "custom",
+      renderCustom: function (swiper, current, total) {
+        return `0${total}`;
+      },
+    },
   });
 
   const swiperText = new Swiper(".slider-text", {
@@ -48,6 +57,8 @@ document.addEventListener("DOMContentLoaded", () => {
   swiperIMG.controller.control = swiperText;
   swiperText.controller.control = swiperIMG;
 
+  //Gear
+
   let gear = document.querySelector(".slider-gear");
 
   swiperText.on("slideNextTransitionStart", function () {
@@ -61,6 +72,34 @@ document.addEventListener("DOMContentLoaded", () => {
     gsap.to(gear, 2.8, {
       rotation: "-=50",
       ease: Power2.easeOut,
+    });
+  });
+
+  let curnum = document.querySelector(".slider-pagination-count .current"),
+    pagcur = document.querySelector(".slider-pagination-current__num");
+
+  swiperText.on("slideChange", function () {
+    let ind = swiperText.realIndex + 1;
+    gsap.to(curnum, 0.2, {
+      force3D: true,
+      y: -10,
+      opacity: 0,
+      ease: Power2.easeOut,
+      onComplete: function () {
+        gsap.to(curnum, 0.1, {
+          force3D: true,
+          y: 10,
+        });
+        curnum.innerHTML = `0${ind}`;
+        pagcur.innerHTML = `0${ind}`;
+      },
+    });
+    gsap.to(curnum, 0.2, {
+      force3D: true,
+      y: 0,
+      opacity: 1,
+      ease: Power2.easeOut,
+      delay: 0.3,
     });
   });
 });
